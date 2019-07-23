13e931c0cc467fab41911fce2152dca9904e41ee
Cheng Hua <44399091+dayAndnight2018@users.noreply.github.com>
2019-07-23 09:05:09 +0800

    Create Singleton.java

---
+++ Singleton.java
@@ -0,0 +1,14 @@
+//防止继承
+public final class Singleton {
+	
+	//配置静态构造函数，防止外部创建对象
+	private Singleton() {}
+	
+	//饿汉式创建单例对象
+	private static final Singleton instance = new Singleton();
+
+	//创建公开的静态方法，用于外部获取对象
+	public static Singleton getInstance() {		
+		return instance;
+	}
+}
